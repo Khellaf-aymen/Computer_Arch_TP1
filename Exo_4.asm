93d2bc888ec6376d87ad21e783a2d6a73aa2ed94
aymen <aymenkhellaf.mi@gmail.com>
2023-12-18 23:00:39 +0100

    Add some message

--- Exo_4.asm
+++ Exo_4.asm
@@ -7,18 +7,19 @@ var: .space 22
 
 main:
 
-
+#read the String
 li $v0,8
-la $a0,var
-li $a1,22
+la $a0,var # Addr to storage 
+li $a1,22  # Bytes of input 
 syscall
 
-
+#Print a new line 
 
 li $a0,10
 li $v0,11
 syscall
 
+#print the String
 la $a0 , var
 li $v0,4
 syscall
@@ -36,7 +37,7 @@ syscall
 
 
 
-
+#Terminat the App
 li $v0 ,10
 syscall 
 .end main
